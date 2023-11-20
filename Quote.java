70206f81a03d924269166ddb03ea82c12d809ed4
PrasadK02 <p.khairnar2002@gmail.com>
2023-11-20 13:10:33 +0530

    fourth commit

---
+++ Quote.java
@@ -0,0 +1,5 @@
+public class Quote {
+    public static void main(String[] args) {
+        System.out.println("The greatest glory in living lies not in never falling, but in rising every time we fall.");
+    }
+}
